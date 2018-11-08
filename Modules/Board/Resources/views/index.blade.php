3f73f0b5565d37d3943658f49b648c5468516611
huueadar <huueadarno1@gmail.com>
2018-11-08 19:36:44 +0700

    Add UI invite user

--- Modules/Board/Resources/views/index.blade.php
+++ Modules/Board/Resources/views/index.blade.php
@@ -1,6 +1,6 @@
 @extends('master')
 @section('title', 'Board')
-@section('page-header', 'Project A')
+@section('page-header') {{ $project_name }} @stop
 @section('cssloader')
 <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
 <style type="text/css">
@@ -27,7 +27,23 @@
 </style>
 @stop
 
+@section('breadcrumb')
+<div class="breadcrumb">
+    <a href="{{ url('/board/create-issue') }}" class="modal-trigger" data-url="{{ url('/board/create-issue') }}"><button type="button" class="btn btn-block btn-success btn-sm">Create issue</button></a>
+</div>
+@stop
+
 @section('content')
+    <div id='my-modal' class="modal fade" tabindex="-1">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <iframe src="" width="100%" scrolling="yes" frameborder="0"></iframe>
+            </div>
+        </div>
+    </div>
+
+
+
     <!-- /.search form -->
     <div class="row after_row">
         <div class="col-md-2">
@@ -43,61 +59,30 @@
         </div>
         <div class="col-md-2">
             <img src="{{ asset('images/icons_user.svg') }}" width="30px" class="img-circle" alt="User Image">
-            <img src="{{ asset('images/add_user_icon.svg') }}" width="30px" class="img-circle" alt="Add member">
+            <img src="{{ asset('images/add_user_icon.svg') }}" width="30px" class="img-circle" alt="Add member" data-toggle="modal" data-target="#formMoiUser">
         </div>
     </div>
 
     <div class="row">
+        @foreach ($issue_statuss as $issue_status)
     	<div class="col-md-2 column">
     		<div class="box">
     			<div class="box-header with-border">
-    				<h3 class="box-title">Backlog</h1>
+    				<h3 class="box-title">{{ $issue_status->name }}</h1>
     			</div>
-    			<div class="box-body">
-                    <ul id="card_list_1" class="connectedSortable todo-list card_list">
-                        <li id="1">Title 1</li>
-                        <li id="2">Title 2</li>
-                        <li id="3">Title 3</li>
+    			<div class="box-body" id={!! $issue_status->id !!}>
+                    <ul id="card_list_1" data="{!! $issue_status->id !!}" class="connectedSortable todo-list card_list">
+                        @foreach ($issues as $issue)
+                            @if ($issue->issue_status == $issue_status->id)
+                                <li id="{!! $issue->id !!}"><a href="{{ url('/board/bug-detail/'.$issue->id) }}" class="modal-trigger" data-url="{{ url('/board/bug-detail/' .$issue->id) }}">{!! $issue->summary !!}</a></li>
+                            @endif
+                        @endforeach
+                        {{-- <li> <a href="">Link tạo issue</a></li> --}}
                     </ul>
                 </div>
     		</div>
     	</div>
-    	<div class="col-md-2 column">
-    		<div class="box">
-    			<div class="box-header with-border">
-    				<h3 class="box-title">Working</h1>
-    			</div>
-    			<div class="box-body">
-                    <ul id="card_list_2" class="connectedSortable todo-list card_list">
-
-                    </ul>
-                </div>
-    		</div>
-    	</div>
-    	<div class="col-md-2 column">
-    		<div class="box">
-    			<div class="box-header with-border">
-    				<h3 class="box-title">Reviewing</h1>
-    			</div>
-    			<div class="box-body">
-                    <ul id="card_list_3" class="connectedSortable todo-list card_list">
-
-                    </ul>
-                </div>
-    		</div>
-    	</div>
-    	<div class="col-md-2 column">
-    		<div class="box">
-    			<div class="box-header with-border">
-    				<h3 class="box-title">Done</h1>
-    			</div>
-    			<div class="box-body">
-                    <ul id="card_list_4" class="connectedSortable todo-list card_list">
-
-                    </ul>
-                </div>
-    		</div>
-    	</div>
+        @endforeach
     	<div class="col-md-1">
     		<button type="button" class="btn btn-default btn-md" title="Create column"> + </button>
     	</div>
@@ -109,3 +94,40 @@
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 <script src="{{ asset('js/dragable.js') }}"></script>
 @stop
+
+<div id="formMoiUser" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Invite To Board</h4>
+      </div>
+      <div class="modal-body">
+        <input type="text" class="form-control" id="usr" placeholder="Email address or name">
+        <br>
+        <textarea class="form-control" rows="5" id="msgInvite"></textarea>
+        <br>
+        <button type="button" class="btn btn-default form-control" id="btnInvite">Send Invitation</button>
+
+        <br>
+        <div style="padding-top: 10px;padding-bottom: 10px">
+            <div style="float: left;">
+                    <span class="glyphicon glyphicon-link" ></span>
+                    <label>Invite With Link</label>
+                </div>
+                <div style="float: right">
+                    <a href="#">Create Link</a>
+            </div>
+        </div>
+        
+        
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal" >Close</button>
+      </div>
+    </div>
+
+  </div>
+</div>
