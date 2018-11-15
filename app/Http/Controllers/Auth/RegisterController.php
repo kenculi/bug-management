329c6714ea88e0aaaca32f5ac5a7e7fdddb4e9e1
Nhokheo <Nhokheo>
2018-11-16 00:28:08 +0700

    [HOTFIX] Set default email to send reset password

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/';
+    protected $redirectTo = '/project';
 
     /**
      * Create a new controller instance.
--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'Dragonblue789@gmail.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'zero9legend@gmail.com'),
         'name' => env('MAIL_FROM_NAME', 'No_Replay'),
     ],
 
